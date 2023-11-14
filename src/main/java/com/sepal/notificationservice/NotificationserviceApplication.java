4f8d195b65bdc7db33a2d55baa95f4563e05426c
Seth Pal <sethpalster@gmail.com>
2023-11-15 01:13:58 +0530

    code cleanup

--- src/main/java/com/sepal/notificationservice/NotificationserviceApplication.java
+++ src/main/java/com/sepal/notificationservice/NotificationserviceApplication.java
@@ -12,7 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.sepal.notificationservice.config.TwilioConfig;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication()
+import java.util.Base64;
+
+@SpringBootApplication
 @Configuration
 public class NotificationserviceApplication {
 
@@ -21,8 +23,7 @@ public class NotificationserviceApplication {
 
 	@Value("${twilio.authToken}")
 	private String auth;
-	@Autowired
-	TwilioConfig twilioConfig;
+
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationserviceApplication.class, args);
 	}
@@ -30,6 +31,18 @@ public class NotificationserviceApplication {
 	@PostConstruct
 	public void setup()
 	{
-		Twilio.init(twilioConfig.decodeValue(accountSSID),twilioConfig.decodeValue(auth));
+		Twilio.init(decodeValue(accountSSID),decodeValue(auth));
+	}
+	public String decodeValue(String encodedValue)
+	{
+		byte[] decodedBytes = Base64.getDecoder().decode(encodedValue);
+		return new String(decodedBytes);
+	}
+
+	public String  encodeValue(String encodedValue) {
+		String BasicBase64format
+				= Base64.getEncoder()
+				.encodeToString(encodedValue.getBytes());
+		return BasicBase64format;
 	}
 }
--- src/test/java/com/sepal/notificationservice/NotificationserviceApplicationTests.java
+++ src/test/java/com/sepal/notificationservice/NotificationserviceApplicationTests.java
@@ -16,29 +16,4 @@ class NotificationserviceApplicationTests {
 
     }
 
-    @Test
-    void encodeDecode()
-    {
-        String sample="ee0841de99efe03d93066ae3b1e086d2";
-        String ecoded =encodeValue(sample);
-        System.out.println("Encoded Value="+ ecoded);
-        String decoded=decodeValue(ecoded);
-        System.out.println("Decoded Value="+ decoded);
-
-
-    }
-    public String decodeValue(String encodedValue)
-    {
-        byte[] decodedBytes = Base64.getDecoder().decode(encodedValue);
-        return new String(decodedBytes);
-    }
-
-    public String  encodeValue(String encodedValue) {
-        String BasicBase64format
-                = Base64.getEncoder()
-                .encodeToString(encodedValue.getBytes());
-        return BasicBase64format;
-    }
-//    AC5527db0838d6a6ebc24206d9a2c85e4e
-   // ee0841de99efe03d93066ae3b1e086d2
 }
